c3d45033cec72da211c7b669f142e216528486f2
shakibthe69 <shakibthe69@gmail.com>
2026-07-16 15:34:33 +0600

    dom

---
+++ java script/18-dom/README.md
@@ -0,0 +1,22 @@
+# Chapter 18 - DOM
+
+## Topics
+
+- DOM
+- getElementById
+- querySelector
+- querySelectorAll
+- innerText
+- innerHTML
+- textContent
+- style
+- createElement
+- appendChild
+- remove
+
+## Mini Projects
+
+- Text Changer
+- Dark Mode
+- Counter
+- Student Profile
---
+++ java script/18-dom/script.js
@@ -0,0 +1,26 @@
+
+
+document.body.style.color =
+"white";
+let btn =
+document.getElementById("btn");
+
+btn.onclick=function(){
+
+document.getElementById("title")
+.innerText =
+"Hello Shakib";
+
+};
+
+document
+.getElementById("darkBtn")
+.onclick=function(){
+
+document.body.style.background=
+"black";
+
+document.body.style.color=
+"white";
+
+};
